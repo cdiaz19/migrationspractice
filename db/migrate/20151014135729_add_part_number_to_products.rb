bb4ab33f47f5857997d161595602e871d6f3a7d4
cdiaz19 <cdiaz@pernix-solutions.com>
2015-10-18 21:55:27 -0600

    initial commit

--- db/migrate/20151014135729_add_part_number_to_products.rb
+++
@@ -1,6 +0,0 @@
-class AddPartNumberToProducts < ActiveRecord::Migration
-  def change
-    add_column :products, :part_number, :string
-    add_index :products, :part_number
-  end
-end
--- db/migrate/20151014141808_add_user_ref_to_products.rb
+++
@@ -1,6 +0,0 @@
-class AddUserRefToProducts < ActiveRecord::Migration
-  def change
-    add_reference :products, :user, index: true
-    add_foreign_key :products, :users
-  end
-end
